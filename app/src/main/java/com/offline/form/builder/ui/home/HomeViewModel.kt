d90b9916cb6414aedfc57688d294d309397f71dd
Ajay <ajay.mathur850@gmail.com>
2022-12-12 21:13:39 +0530

    fixes

--- app/src/main/java/com/offline/form/builder/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/offline/form/builder/ui/home/HomeViewModel.kt
@@ -10,28 +10,202 @@ class HomeViewModel : ViewModel() {
     val questions: List<Question> by lazy {
         listOf(
             Question(
-                id = "Input 1",
-                question = "What is your name",
+                id = "A1",
+                question = "Name of the Respondent",
                 options = listOf(
-                    OptionType.InputField(InputType.TYPE_CLASS_NUMBER, "Enter your name")
+                    OptionType.InputField(InputType.TYPE_CLASS_TEXT, "Enter your name")
                 ),
                 validate = StringInputValidation(),
                 optionType = OptionTypeEnum.INPUT
             ),
             Question(
-                id = "Input 2",
-                question = "What is my name",
+                id = "A2",
+                question = " District Name ",
+                options = listOf(
+                    OptionType.InputField(InputType.TYPE_CLASS_TEXT, "Enter District Name")
+                ),
+                validate = StringInputValidation(),
+                optionType = OptionTypeEnum.INPUT
+            ),
+            Question(
+                id = "A3",
+                question = "Block Name",
+                options = listOf(
+                    OptionType.InputField(InputType.TYPE_CLASS_TEXT, "Enter name of Block")
+                ),
+                validate = StringInputValidation(),
+                optionType = OptionTypeEnum.INPUT
+            ),
+            Question(
+                id = "A4",
+                question = "Agri-Camp Name",
+                options = listOf(
+                    OptionType.InputField(InputType.TYPE_CLASS_TEXT, "Enter Agri-Camp Name")
+                ),
+                validate = StringInputValidation(),
+                optionType = OptionTypeEnum.INPUT
+            ),
+            Question(
+                id = "A5",
+                question = "Village Name",
+                options = listOf(
+                    OptionType.InputField(InputType.TYPE_CLASS_TEXT, "Enter Village Name")
+                ),
+                validate = StringInputValidation(),
+                optionType = OptionTypeEnum.INPUT
+            ),
+            Question(
+                id = "A6",
+                question = "Household Serial No.",
+                options = listOf(
+                    OptionType.InputField(InputType.TYPE_CLASS_TEXT, "Enter Household Serial No.")
+                ),
+                validate = StringInputValidation(),
+                optionType = OptionTypeEnum.INPUT
+            ),
+            Question(
+                id = "A7",
+                question = "Consent from the Respondent",
                 options = listOf(
                     OptionType.CheckBox(
                         listOf(
-                            CheckBoxItems("ID1", "Option 1"),
-                            CheckBoxItems("ID2", "Option 2")
+                            CheckBoxItems("1", "Yes"),
+                            CheckBoxItems("1", "No")
                         )
                     )
                 ),
                 validate = CheckboxInputValidation(),
                 optionType = OptionTypeEnum.CHECK_BOX
+            ),
+
+            Question(
+                id = "A8",
+                question = " Age of Respondent",
+                options = listOf(
+                    OptionType.InputField(InputType.TYPE_CLASS_TEXT, "Enter Age of Respondent")
+                ),
+                validate = StringInputValidation(),
+                optionType = OptionTypeEnum.INPUT
+            ),
+            Question(
+                id = "A9",
+                question = "Gender of the Respondent",
+                options = listOf(
+                    OptionType.CheckBox(
+                        listOf(
+                            CheckBoxItems("1", "Male"),
+                            CheckBoxItems("2", "Female")
+                        )
+                    )
+                ),
+                validate = CheckboxInputValidation(),
+                optionType = OptionTypeEnum.CHECK_BOX
+            ),
+            Question(
+                id = "A10",
+                question = "Mobile number of respondent / House Hold member",
+                options = listOf(
+                    OptionType.InputField(InputType.TYPE_CLASS_TEXT, "Enter Mobile number of respondent / House Hold member")
+                ),
+                validate = StringInputValidation(),
+                optionType = OptionTypeEnum.INPUT
+            ),
+            Question(
+                id = "A11",
+                question = "Are you Head of the Family",
+                options = listOf(
+                    OptionType.CheckBox(
+                        listOf(
+                            CheckBoxItems("1", "Yes"),
+                            CheckBoxItems("2", "No")
+                        )
+                    )
+                ),
+                validate = CheckboxInputValidation(),
+                optionType = OptionTypeEnum.CHECK_BOX
+            ),
+
+            //ADD  A.12 Name of the family Head (if response of A.10 is (2))
+            Question(
+                id = "A13",
+                question = "Gender of the head of Household",
+                options = listOf(
+                    OptionType.CheckBox(
+                        listOf(
+                            CheckBoxItems("1", "Male"),
+                            CheckBoxItems("2", "Female")
+                        )
+                    )
+                ),
+                validate = CheckboxInputValidation(),
+                optionType = OptionTypeEnum.CHECK_BOX
+            ),
+            Question(
+                id = "A14",
+                question = "Relationship of respondent to Head of HH",
+                options = listOf(
+                    OptionType.CheckBox(
+                        listOf(
+                            CheckBoxItems("1", " Head of Household"),
+                            CheckBoxItems("2", "Spouse"),
+                            CheckBoxItems("3", "Child (own/step)"),
+                            CheckBoxItems("4", "Parent /Parent in law"),
+                            CheckBoxItems("5", "Brother/sister"),
+                            CheckBoxItems("6", "Other relatives"),
+                            CheckBoxItems("7", "Unrelated"),
+
+                        )
+                    )
+                ),
+                validate = CheckboxInputValidation(),
+                optionType = OptionTypeEnum.CHECK_BOX
+            ),
+            Question(
+                id = "A15",
+                question = "Marital status of respondent",
+                options = listOf(
+                    OptionType.CheckBox(
+                        listOf(
+                            CheckBoxItems("1", "Single (never married)"),
+                            CheckBoxItems("2", "Monogamously Married"),
+                            CheckBoxItems("3", "Polygamously Married"),
+                            CheckBoxItems("4", "Divorced"),
+                            CheckBoxItems("5", "Widowed"),
+                            CheckBoxItems("6", "Separated"),
+                            CheckBoxItems("7", "Cohabiting"),
+
+                            )
+                    )
+                ),
+                validate = CheckboxInputValidation(),
+                optionType = OptionTypeEnum.CHECK_BOX
+            ),
+            Question(
+                id = "A16",
+                question = "Highest Education level of respondent",
+                options = listOf(
+                    OptionType.CheckBox(
+                        listOf(
+                            CheckBoxItems("1", "No Formal Education"),
+                            CheckBoxItems("2", "Less than Grade 5"),
+                            CheckBoxItems("3", "Less than grade 10"),
+                            CheckBoxItems("4", "Less than Grade 12"),
+                            CheckBoxItems("5", "College Student"),
+                            CheckBoxItems("6", "University Undergraduate Student"),
+                            CheckBoxItems("7", "Tertiary Certificate;Diploma"),
+                            CheckBoxItems("8", "Bachelors Degree"),
+                            CheckBoxItems("9", "Masters Degree and Above."),
+
+                            )
+                    )
+                ),
+                validate = CheckboxInputValidation(),
+                optionType = OptionTypeEnum.CHECK_BOX
             )
+
+
+
+            
         )
     }
 
