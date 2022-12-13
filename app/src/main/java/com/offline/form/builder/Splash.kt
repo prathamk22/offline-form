16b28967895d7046aafa572259fd2bdeda39976f
Ajay Mathur <ajayathur850@gmail.com>
2022-12-13 13:37:34 +0530

    Splash Screen Updated

--- app/src/main/java/com/offline/form/builder/Splash.kt
+++ app/src/main/java/com/offline/form/builder/Splash.kt
@@ -1,17 +1,25 @@
 package com.offline.form.builder
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.text.Html
 import android.widget.Button
-
+import android.widget.TextView
+@Suppress("DEPRECATION")
 class Splash : AppCompatActivity() {
+    private lateinit var textView:TextView
 
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
         supportActionBar?.hide()
+        textView=findViewById(R.id.surveyText)
+        textView.text=Html.fromHtml("Programme Name: <b>Cashew Infrastructure Development Project (CIDP)</b> \nClient Name: <b>Ministry of Agriculture, Govt of the Republic of Zambia</b>  \n<b>Survey for Beneficiary Impact Assessment (BIA) of CIDP project</b>  \n<b>Survey By: SPA Capital Advisors Pvt. Ltd. & KCM Green Ltd.</b> ")
 
         val start_btn=findViewById(R.id.btnStart) as Button
         start_btn.setOnClickListener{
