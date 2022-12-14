6b127645915e9af587f803ad35e14991a66ad1f6
Ajay <ajay.mathur850@gmail.com>
2022-12-14 12:03:28 +0530

    Section F added

--- app/src/main/java/com/offline/form/builder/ui/CropProfileFragment.kt
+++ app/src/main/java/com/offline/form/builder/ui/CropProfileFragment.kt
@@ -27,9 +27,59 @@ class CropProfileFragment : BaseTableFragment() {
         forms.add(
             Form(
                 isMandatory = true,
+                formType = FormTypes.SINGLE_LINE_TEXT,
+                question = "F2 Crop Name",
+                hint = "please enter Crop Name",
+                singleLineTextType = SingleLineTextType.TEXT,
+                errorMessage = "Please choose"
+            )
+        )
+        forms.add(
+            Form(
+                isMandatory = true,
                 formType = FormTypes.NUMBER,
                 question = "F3 Area (in Ha)",
                 hint = "please enter your membership",
+                singleLineTextType = SingleLineTextType.TEXT,
+                errorMessage = "Please provide an answer"
+            )
+        )
+        forms.add(
+            Form(
+                isMandatory = true,
+                formType = FormTypes.NUMBER,
+                question = "F4 Total Production (in Kg) ",
+                hint = "please enter Total Production (in Kg)",
+                singleLineTextType = SingleLineTextType.TEXT,
+                errorMessage = "Please provide an answer"
+            )
+        )
+        forms.add(
+            Form(
+                isMandatory = true,
+                formType = FormTypes.NUMBER,
+                question = "F5 Marketable surplus (in % of total production)  ",
+                hint = "please enter Marketable surplus",
+                singleLineTextType = SingleLineTextType.TEXT,
+                errorMessage = "Please provide an answer"
+            )
+        )
+        forms.add(
+            Form(
+                isMandatory = true,
+                formType = FormTypes.NUMBER,
+                question = "F6 Selling price (Kwacha per Kg)",
+                hint = "please enter Selling price",
+                singleLineTextType = SingleLineTextType.TEXT,
+                errorMessage = "Please provide an answer"
+            )
+        )
+        forms.add(
+            Form(
+                isMandatory = true,
+                formType = FormTypes.NUMBER,
+                question = "F7 Quantity sold ",
+                hint = "please enter Quantity sold ",
                 singleLineTextType = SingleLineTextType.TEXT,
                 errorMessage = "Please provide an answer"
             )
